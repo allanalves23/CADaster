2a158948a48e3ae970155efd4dad39ac22fe74c7
Allan Wanderley Alves <allanalves23@outlook.com>
2017-09-05 02:37:30 -0300

    Base do sistema estabelecida b0.2

---
+++ src/view/Usuario.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author areznla
+ */
+public class Usuario {
+    private String nome;
+    private char sexo;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
